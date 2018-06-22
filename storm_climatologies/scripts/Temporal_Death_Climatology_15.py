fa5c236ce84cbcf5e01b89aee6a2abfa78932c88
Aodhan Sweeney <40372081+AodhanSweeney@users.noreply.github.com>
2018-06-22 15:20:23 -0500

    Update Temporal_Death_Climatology_15.py

--- storm_climatologies/scripts/Temporal_Death_Climatology_15.py
+++ storm_climatologies/scripts/Temporal_Death_Climatology_15.py
@@ -49,45 +49,6 @@ def _get_dates_needed(working_date_index, num_dates, climatology_type):
     return numpy.array(date_needed_indices, dtype=int)
 
 
-
-
-def _get_storm_id_and_hour_strings(storm_ids, unix_times_sec):
-    """Turns each pair of storm ID and time into string with storm ID and hour.
-
-    N = number of storm objects
-
-    :param storm_ids: length-N list of storm IDs (strings).
-    :param unix_times_sec: length-N numpy array of valid times.
-    :return: storm_id_and_hour_strings: length-N list of strings.
-    """
-    
-    """no_date_ids = []
-    for x in storm_ids:
-        a = x.split('_')
-        no_date_ids.append(a[0])"""
-    hour_strings = [time_conversion.unix_sec_to_string(t, HOUR_FORMAT)
-                    for t in unix_times_sec]
-
-    return ['{0:s}_{1:s}'.format( hour_strings[i])
-            for i in range(len(storm_ids))]
-
-def _parse_storm_id(storm_id):
-    """Parses hour from each string created by `_get_storm_id_and_hour_strings`.
-
-    N = number of storm objects
-
-    :param storm_id_and_hour_strings: length-N list of strings created by
-        `_get_storm_id_and_hour_strings`.
-    :return: hour_integers: length-N numpy array of hours.
-    """
-    storm_strings = []
-    for s in storm_id:
-        a = s.split('_')
-        storm_strings.append(a[0])
-    
-    return numpy.array(storm_strings)
-
-
 if __name__ == '__main__':
     spc_date_strings = time_conversion.get_spc_dates_in_range(
         first_spc_date_string=FIRST_SPC_DATE_STRING,
