ef0769a24901e1859ea51fbf6635cbad93580b0d
Aodhan Sweeney <40372081+AodhanSweeney@users.noreply.github.com>
2018-06-25 18:29:01 -0500

    Update Temporal_Birth_Climatology_15.py

--- storm_climatologies/scripts/Temporal_Birth_Climatology_15.py
+++ storm_climatologies/scripts/Temporal_Birth_Climatology_15.py
@@ -57,31 +57,7 @@ if __name__ == '__main__':
     num_storm_objects_by_hour = numpy.full(NUM_HOURS_PER_DAY, 0, dtype=int)
 
     for working_date_index in range(num_spc_dates):
-        date_in_memory_indices = _get_dates_needed(working_date_index=working_date_index, 
-                                                   num_dates=num_spc_dates,
-                                                   climatology_type=BIRTH_CLIMATOLOGY_TYPE)
-        for i in range(num_spc_dates):
-            if i in date_in_memory_indices:
-                if storm_object_table_by_spc_date[i] is None:
-                    these_tracking_file_names = (tracking_io.find_processed_files_one_spc_date(
-                                                    spc_date_string=spc_date_strings[i],
-                                                    data_source='segmotion',
-                                                    top_processed_dir_name=TOP_PROCESSED_DIR_NAME,
-                                                    tracking_scale_metres2=TRACKING_SCALE_METRES2))
-                    storm_object_table_by_spc_date[i] = (tracking_io.read_many_processed_files(
-                                                        these_tracking_file_names))
-
-            else:
-                print 'Clearing data for SPC date "{0:s}"...'.format(spc_date_strings[i])
-                storm_object_table_by_spc_date[i] = None
-
-        for j in date_in_memory_indices[1:]:
-            storm_object_table_by_spc_date[j], _ = (storm_object_table_by_spc_date[j].align(
-                                                    storm_object_table_by_spc_date[date_in_memory_indices[0]], axis=1))
-
-        storm_object_tables_to_concat = [storm_object_table_by_spc_date[j] for j in date_in_memory_indices]
-        multiday_storm_object_table = pandas.concat(storm_object_tables_to_concat, axis=0, ignore_index=True)
-        multiday_storm_object_table = multiday_storm_object_table[multiday_storm_object_table['age_sec'] > 900]
+        multiday_storm_object_table = utils.get_storm_object_table(num_spc_dates, Birth_Climatology, working_date_index)
         multiday_storm_object_table= multiday_storm_object_table.reset_index()
         del multiday_storm_object_table['index']
         multi_storm_ids, place_holder = numpy.unique(multiday_storm_object_table['storm_id'].values, return_index = True)
@@ -93,7 +69,7 @@ if __name__ == '__main__':
         
         unique_times = []
         for x in unique_storm_times:
-            if (max(unique_storm_times) - 86800)<= x <= max(unique_storm_times):
+            if (max(unique_storm_times) - 86400)<= x <= max(unique_storm_times):
                 unique_times.append(x)
         
         print('sift 2',len(unique_times))
