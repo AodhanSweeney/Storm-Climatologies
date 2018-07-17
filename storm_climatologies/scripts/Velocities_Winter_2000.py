f407dc50f68a3c6ff1f7d2518f49cbef958191be
Aodhan Sweeney <40372081+AodhanSweeney@users.noreply.github.com>
2018-07-17 15:07:09 -0500

    Update Velocities_Winter_2000.py

--- storm_climatologies/scripts/Velocities_Winter_2000.py
+++ storm_climatologies/scripts/Velocities_Winter_2000.py
@@ -17,62 +17,6 @@ BIRTH_CLIMATOLOGY_TYPE = 'birth'
 DEATH_CLIMATOLOGY_TYPE = 'death'
 PASSAGE_CLIMATOLOGY_TYPE = 'passage'
 
-def _get_dates_needed(working_date_index, num_dates, climatology_type):
-    """Gets dates needed for the given working date.
-
-    :param working_date_index: Array index for the day currently being worked
-        on.
-    :param num_dates: Number of dates total.
-    :return: date_needed_indices: 1-D numpy array with indices of dates needed.
-    """
-
-    if climatology_type == PASSAGE_CLIMATOLOGY_TYPE:
-        return numpy.array([working_date_index], dtype=int)
-
-    date_needed_indices = []
-    if climatology_type == BIRTH_CLIMATOLOGY_TYPE and working_date_index != 0:
-        date_needed_indices.append(working_date_index - 1)
-
-    date_needed_indices.append(working_date_index)
-
-    if (climatology_type == DEATH_CLIMATOLOGY_TYPE and
-            working_date_index != num_dates - 1):
-        date_needed_indices.append(working_date_index + 1)
-
-    return numpy.array(date_needed_indices, dtype=int)
-
-
-def _get_storm_id_and_hour_strings(storm_ids, unix_times_sec):
-    """Turns each pair of storm ID and time into string with storm ID and hour.
-
-    N = number of storm objects
-
-    :param storm_ids: length-N list of storm IDs (strings).
-    :param unix_times_sec: length-N numpy array of valid times.
-    :return: storm_id_and_hour_strings: length-N list of strings.
-    """
-
-    hour_strings = [time_conversion.unix_sec_to_string(t, HOUR_FORMAT)
-                    for t in unix_times_sec]
-
-    return ['{0:s}_{1:s}'.format(storm_ids[i], hour_strings[i])
-            for i in range(len(storm_ids))]
-
-
-def _parse_storm_id_and_hour_strings(storm_id_and_hour_strings):
-    """Parses hour from each string created by `_get_storm_id_and_hour_strings`.
-
-    N = number of storm objects
-
-    :param storm_id_and_hour_strings: length-N list of strings created by
-        `_get_storm_id_and_hour_strings`.
-    :return: hour_integers: length-N numpy array of hours.
-    """
-
-    hour_strings = [s.split('_')[-1] for s in storm_id_and_hour_strings]
-    return numpy.array([int(s) for s in hour_strings], dtype=int)
-
-
 if __name__ == '__main__':
     spc_date_strings = time_conversion.get_spc_dates_in_range(
         first_spc_date_string=FIRST_SPC_DATE_STRING,
@@ -84,7 +28,7 @@ if __name__ == '__main__':
     Total_EV_List =[]
     Total_NV_List =[]
     for working_date_index in range(num_spc_dates):
-        date_in_memory_indices = _get_dates_needed(
+        date_in_memory_indices = utils._get_dates_needed(
             working_date_index=working_date_index, num_dates=num_spc_dates,
             climatology_type=PASSAGE_CLIMATOLOGY_TYPE)
 
@@ -121,9 +65,6 @@ if __name__ == '__main__':
         storm_object_tables_to_concat = [storm_object_table_by_spc_date[j] for j in date_in_memory_indices]
         multiday_storm_object_table = pandas.concat(storm_object_tables_to_concat, axis=0, ignore_index=True)
         mature_table = multiday_storm_object_table[multiday_storm_object_table['age_sec']>=900]
-        """multiday_storm_object_table= multiday_storm_object_table.reset_index()
-        del multiday_storm_object_table['index']"""
-        #display(multiday_storm_object_table)
         storms = []
         for x in multiday_storm_object_table['storm_id']:
             if x in list(mature_table['storm_id']):
