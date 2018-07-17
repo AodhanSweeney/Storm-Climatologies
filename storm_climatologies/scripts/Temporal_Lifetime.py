f423b817950b65d426cc1b42531b7c745615caea
Aodhan Sweeney <40372081+AodhanSweeney@users.noreply.github.com>
2018-07-17 15:08:12 -0500

    Update Temporal_Lifetime.py

--- storm_climatologies/scripts/Temporal_Lifetime.py
+++ storm_climatologies/scripts/Temporal_Lifetime.py
@@ -47,7 +47,7 @@ if __name__ == '__main__':
     storm_ages = []
 
     for working_date_index in range(num_spc_dates):
-        date_in_memory_indices = _get_dates_needed(working_date_index=working_date_index, num_dates=num_spc_dates)
+        date_in_memory_indices = utils._get_dates_needed(working_date_index=working_date_index, num_dates=num_spc_dates)
         for i in range(num_spc_dates):
             if i in date_in_memory_indices:
                 if storm_object_table_by_spc_date[i] is None:
